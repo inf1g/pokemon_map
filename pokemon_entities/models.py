4cf5a21180b37f7ac70a1e62ae1305b70cba890c
inf <manskiy2k@gmail.com>
2024-08-21 20:46:07 +0300

    создал Pokemon model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,10 @@
 from django.db import models  # noqa F401
 
-# your models here
+
+class Pokemon(models.Model):
+    description = models.TextField(max_length=200, help_text='Описание покемона.')
+    title = models.CharField(max_length=200, help_text='Название покемона.')
+    created_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.description} {self.title} {self.created_date}'
