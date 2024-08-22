3b39ebc9b44cd545d4238d7a62d467e266c2d44d
inf <manskiy2k@gmail.com>
2024-08-22 16:45:46 +0300

    добавил описания полей для классов PokemonEntity и Pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -9,6 +9,7 @@ class Pokemon(models.Model):
     )
     photo = models.ImageField(
         upload_to='Pokemon',
+        help_text='Картинка покемона',
         null=True,
         blank=True
     )
@@ -22,21 +23,17 @@ class PokemonEntity(models.Model):
         Pokemon,
         on_delete=models.CASCADE,
         related_name='entities',
-        help_text='Связанный покемон.'
+        help_text='Название покемона.'
     )
-    lat = models.FloatField(
-        help_text='Широта местоположения покемона.'
-    )
-    lon = models.FloatField(
-        help_text='Долгота местоположения покемона.'
-    )
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(null=True, blank=True)
-    health = models.IntegerField(null=True, blank=True)
-    attack = models.IntegerField(null=True, blank=True)
-    defense = models.IntegerField(null=True, blank=True)
-    stamina = models.IntegerField(null=True, blank=True)
+    lat = models.FloatField(help_text='Широта местоположения покемона.')
+    lon = models.FloatField(help_text='Долгота местоположения покемона.')
+    appeared_at = models.DateTimeField(help_text='Появится в.', null=True, blank=True)
+    disappeared_at = models.DateTimeField(help_text='Исчезнет в.', null=True, blank=True)
+    level = models.IntegerField(help_text='Уровень.', null=True, blank=True)
+    health = models.IntegerField(help_text='Здоровье', null=True, blank=True)
+    attack = models.IntegerField(help_text='Атака', null=True, blank=True)
+    defense = models.IntegerField(help_text='Защита', null=True, blank=True)
+    stamina = models.IntegerField(help_text='Выносливость', null=True, blank=True)
 
     def __str__(self):
         return f'{self.pokemon.title} на ({self.lat}, {self.lon})'
